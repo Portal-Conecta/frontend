3876cf72481908213c412b60b6e8d8c4641aa7b8
PabloTzeliks <arq.pabloo@gmail.com>
2026-07-08 16:07:50 -0300

    #265 feat: adiciona molecula Section de selecao unica

--- packages/ui/src/molecules/index.ts
+++ packages/ui/src/molecules/index.ts
@@ -2,5 +2,6 @@ export * from "./Alert";
 export * from "./ErrorPage";
 export * from "./Field";
 export * from "./SearchBar";
+export * from "./Section";
 export * from "./Select";
 export * from "./SidebarNavItem";
