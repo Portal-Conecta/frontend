50ce3bb18188f69fa20f3e0f54fdc60f7a430d50
melissarfaela <melissa_r_pereira@estudante.sesisenai.org.br>
2026-07-17 15:27:30 -0300

    #309 refactor: colapsa BaseFeedbackModal no SuccessModal

--- packages/checklist/src/components/index.ts
+++ packages/checklist/src/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./molecules/RoomChecklistItem/index";
 export * from "./atoms/StatusToggle";
 export * from "./molecules/ChecklistItem";
-export * from "./molecules/BaseFeedbackModal";
 export * from "./molecules/SuccessModal";
