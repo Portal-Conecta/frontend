0025f22738a99b52aee99468ffff981376872c37
danielsismer <daniel_sismer@estudante.sesisenai.org.br>
2026-07-17 19:26:47 -0300

    #100 feat: adiciona ChecklistFilters (periodo, turma, sala)

--- packages/checklist/src/components/index.ts
+++ packages/checklist/src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./molecules/ChecklistItemResult";
 export * from "./molecules/RoomChecklistItem/index";
 export * from "./atoms/StatusToggle";
 export * from "./molecules/ChecklistItem";
+export * from "./organisms/ChecklistFilters";
